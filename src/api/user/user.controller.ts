8f709fe0f866992febcfac2abd6a25837900f044
gyomdyung <seastory624@gmail.com>
2022-08-31 19:55:11 +0900

    Feat: User API 스켈레톤 코드 작성

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // 회원가입
+  @Post('/signup')
+  signup(@Body() createUserDto: CreateUserDto) {
+    return this.userService.signup(createUserDto);
+  }
+
+  // 로그인
+  @Post('/login')
+  login(email: string, password: string) {
+    return this.userService.login(email, password);
+  }
+
+  // 회원탈퇴
+  @Delete('/')
+  deleteUser(email: string, password: string) {
+    return this.userService.deleteUserByEmail(email, password);
+  }
+}
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  // constructor() {}
+
+  signup(createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+
+  login(email: string, password: string) {
+    return { email, password };
+  }
+
+  deleteUserByEmail(email: string, password: string) {
+    return { email, password };
+  }
+}
