8a9eb610d7bdfef6eddcc9a49e74f80c30292d59
gyomdyung <seastory624@gmail.com>
2022-08-31 20:12:01 +0900

    Feat: 회원가입 API Demo

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,12 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
-  // constructor() {}
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
-  signup(createUserDto: CreateUserDto) {
-    return createUserDto;
+  async signup(createUserDto: CreateUserDto) {
+    const { email, name, password, age, gender } = createUserDto;
+
+    if (await this.userRepository.findOne({ where: { email } }))
+      throw new ConflictException(
+        `이미 가입한 이메일입니다. 다른 계정으로 회원가입 해주세요.`,
+      );
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({
+      email,
+      name,
+      password: hashedPassword,
+      age,
+      gender,
+    });
+    const result = await this.userRepository.save(user);
+
+    return result;
   }
 
   login(email: string, password: string) {
