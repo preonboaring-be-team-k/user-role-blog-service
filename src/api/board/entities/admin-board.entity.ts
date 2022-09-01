106413152e08a5cdcd7a3e07f0ddd45eb7bdf35a
chanbokW <cksqhr4961@naver.com>
2022-09-01 11:15:22 +0900

    Feat: [#2] admin-board Entity 생성

---
+++ src/api/board/entities/admin-board.entity.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class AdminBoard {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    discription: string
+
+    @CreateDateColumn()
+    createAt: Date;
+
+    @UpdateDateColumn()
+    updateAt?: Date;
+
+    @DeleteDateColumn()
+    deleteAt?: Date | null;
+
+    @Column()
+    isDeleted: boolean;
+
+
+}
