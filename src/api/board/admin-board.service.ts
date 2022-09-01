fa5f1249ca5254c169586b4d68ab10bee80a8b56
chanbokW <cksqhr4961@naver.com>
2022-09-01 12:20:57 +0900

    Feat: [#06] admin-board 게시물 수정

--- src/api/board/admin-board.service.ts
+++ src/api/board/admin-board.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { BoardResponseDto } from "./dtos/boardResponse.dto";
 import { CreateBoardDto } from "./dtos/createBoard.dto";
+import { UpdateBoardDto } from "./dtos/updateBoard.dto";
 import { AdminBoard } from "./entities/admin-board.entity";
 
 @Injectable()
@@ -32,5 +33,16 @@ export class AdminBoardService {
         }
 
         return BoardResponseDto.of(board);
+    }
+
+    async editBoard(id:number,updateBoardDto: UpdateBoardDto) {
+        const board: AdminBoard = await this.adminBoardRepository.findOne({
+            where: { id }
+        });
+        if (!board || board.isDeleted) {
+            throw new NotFoundException('존재하지 않은 게시물입니다.');
+        }
+       
+        return await this.adminBoardRepository.save(board.update(updateBoardDto));
     }
 }
