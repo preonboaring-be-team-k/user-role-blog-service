b0621efc09baaa57f9e6c37f1b377ebf47b802e8
chanbokW <cksqhr4961@naver.com>
2022-09-01 19:33:12 +0900

    Feat: [#08] admin-board Swagger api문서 작성

--- src/api/board/dtos/createBoard.dto.ts
+++ src/api/board/dtos/createBoard.dto.ts
@@ -1,6 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateBoardDto {
 
+    @ApiProperty({
+        example: '웨인힐스벤처스의 운영계획입니다.',
+        description: '게시물 제목',
+        required: true
+    })
+    @IsNotEmpty()
+    @IsString()
     title: string;
+
+    @ApiProperty({
+        example: '웨인힐스벤처스의 운영계획은 ~~~~!!! .',
+        description: '게시물 내용',
+        required: true
+    })
+    @IsNotEmpty()
+    @IsString()
     discription: string;
     // role
 
