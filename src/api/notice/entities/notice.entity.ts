d7d5155c145c5705982db06a144c5f2f60ed77f1
Ksanbal <dev.ksanbal@gmail.com>
2022-09-02 15:09:34 +0900

    Fix: [#24] User 모델 이름 변경 버그 수정

--- src/api/notice/entities/notice.entity.ts
+++ src/api/notice/entities/notice.entity.ts
@@ -1,20 +1,26 @@
-import { User } from "../../user/entities/user.entity";
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { UserEntity } from '../../user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Notice {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    title: string
-          
-    @Column()
-    description: string
+  @Column()
+  title: string;
 
-    @CreateDateColumn()
-    createAt: Date
-    
-    @ManyToOne(() => User, { eager: true })
-    user: User
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @ManyToOne(() => UserEntity, { eager: true })
+  user: UserEntity;
 }
