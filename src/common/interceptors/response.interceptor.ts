e67767450700beddb09ebeea0fd42cd5193128b4
Ksanbal <dev.ksanbal@gmail.com>
2022-09-02 16:50:37 +0900

    Feat: [#29] / Exception filter, Interceptor 적용

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+const SucessStatusCodeMessage = {
+  200: 'OK',
+  201: 'Created',
+  202: 'Accepted',
+  203: 'Non-Authoritative Information',
+  204: 'No Content',
+  205: 'Reset Content',
+  206: 'Partial Content',
+};
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const statusCode = context.switchToHttp().getResponse().statusCode;
+
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        timestamp: new Date(),
+        message: SucessStatusCodeMessage[statusCode],
+        data,
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // HttpException Filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Response Interceptor
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   // Swagger
   SwaggerModule.setup(
