c742cb402901e51d310ce8e366de3b9751f0c88a
gyomdyung <seastory624@gmail.com>
2022-09-05 11:01:01 +0900

    Feat: user 데코레이터 예외 추가

--- src/api/auth/decorator/user.decorator.ts
+++ src/api/auth/decorator/user.decorator.ts
@@ -1,8 +1,16 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  HttpException,
+} from '@nestjs/common';
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+
+    if (request.user === undefined)
+      throw new HttpException('로그인 후 이용해주시기 바랍니다.', 400);
+
     return request.user;
   },
 );
